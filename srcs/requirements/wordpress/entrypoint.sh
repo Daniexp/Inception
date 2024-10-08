3277b17f23a2461436585b8fb438deb91efb268a
dexposit <dexposit@Debian.myguest.virtualbox.org>
2024-10-08 19:15:20 +0200

    install wordpress successfully with mariadb volume and starting nginx config

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -2,18 +2,18 @@ if [ ! "$(ls -A "$WPVLM")" ]; then
 	sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf &&  \
 	chmod +rwx $WPVLM && 
 	wp core download --path="$WPVLM" --allow-root &&
-	echo "<?php \n
-	define( 'DB_NAME', '$DBNAME' );\n
-	define( 'DB_USER', '$DBUSER' );\n
-	define( 'DB_PASSWORD', '$DBUSERPASS' );\n
-	define( 'DB_HOST', '$DBHOST' );\n
-	define( 'DB_CHARSET', 'utf8mb4' );\n
-	define( 'DB_COLLATE', '' );\n
-	\$table_prefix = 'wp_';\n
-	define( 'WP_DEBUG', false );\n
-	if ( ! defined( 'ABSPATH' ) ) {\n
-		define( 'ABSPATH', __DIR__ . '/' );\n
-	}\n
+	echo "<?php
+	define( 'DB_NAME', '$DBNAME' );
+	define( 'DB_USER', '$DBUSER' );
+	define( 'DB_PASSWORD', '$DBUSERPASS' );
+	define( 'DB_HOST', '$DBHOST' );
+	define( 'DB_CHARSET', 'utf8mb4' );
+	define( 'DB_COLLATE', '' );
+	\$table_prefix = 'wp_'
+	define( 'WP_DEBUG', false );n
+	if ( ! defined( 'ABSPATH' ) ) {
+		define( 'ABSPATH', __DIR__ . '/' );
+	}
 	require_once ABSPATH . 'wp-settings.php'; \
 	" > /var/www/html/wordpress/wp-config.php && 
 	wp core install  --allow-root --url="$DOMAIN" --title="DEXPOSIT HOME" --admin_user="$WPSUPER" --admin_password="$WPSUSERPASS" --admin_email="$EMAIL" --skip-email --path="$WPVLM" && wp user create $WPUSER $USEREMAIL --user_pass="$WPUSERPASS" --allow-root --role=subscriber --path="$WPVLM";
