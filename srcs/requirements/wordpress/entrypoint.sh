501f09003ffbf0f0ee5d11e1f564e0f85c18cf44
dexposit <dexposit@Debian.myguest.virtualbox.org>
2024-10-08 22:12:20 +0200

    a falta de temas de wordpress

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,4 +1,4 @@
-if [ ! "$(ls -A "$WPVLM")" ]; then 
+if [ ! "$(ls -A "$WPVLM/wordpress")" ]; then 
 	sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf &&  \
 	chmod +rwx $WPVLM && 
 	wp core download --path="$WPVLM" --allow-root &&
@@ -9,13 +9,13 @@ if [ ! "$(ls -A "$WPVLM")" ]; then
 	define( 'DB_HOST', '$DBHOST' );
 	define( 'DB_CHARSET', 'utf8mb4' );
 	define( 'DB_COLLATE', '' );
-	\$table_prefix = 'wp_'
-	define( 'WP_DEBUG', false );n
+	"'$table_prefix'" = 'wp_';
+	define( 'WP_DEBUG', false );
 	if ( ! defined( 'ABSPATH' ) ) {
 		define( 'ABSPATH', __DIR__ . '/' );
 	}
 	require_once ABSPATH . 'wp-settings.php'; \
-	" > /var/www/html/wordpress/wp-config.php && 
+	" > $WPVLM/wp-config.php && 
 	wp core install  --allow-root --url="$DOMAIN" --title="DEXPOSIT HOME" --admin_user="$WPSUPER" --admin_password="$WPSUSERPASS" --admin_email="$EMAIL" --skip-email --path="$WPVLM" && wp user create $WPUSER $USEREMAIL --user_pass="$WPUSERPASS" --allow-root --role=subscriber --path="$WPVLM";
 fi
 
